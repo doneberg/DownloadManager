9610bb86419e29911d184774381d1ea3e0de6a28
Don <donde@Dons-MacBook-Air.local>
2026-03-24 11:34:16 -0700

    coded Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DownloadManager.Core;
+
+var downloads = new List<(string, string)>
+{
+    //links go here
+    ("https://github.com/doneberg/DownloadManager/archive/refs/heads/main.zip", "DownloadManager-main.zip"),
+};
+
+using var monitor = new QueueMonitor();
+monitor.Start();
+
+var manager = new DownloadManager.Core.DownloadManager(maxConcurrent: 2);
+
+monitor.Log("Starting downloads...");
+await manager.RunAllAsync(downloads);
+monitor.Log("Finished downloads.");
+monitor.Stop();
