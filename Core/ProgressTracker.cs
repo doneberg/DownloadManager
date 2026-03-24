fd7ae60d5d12b6eec1118b96d24aba564fd8460e
Don <donde@Dons-MacBook-Air.local>
2026-03-24 11:43:29 -0700

    ProgressTracker coded

--- Core/ProgressTracker.cs
+++ Core/ProgressTracker.cs
@@ -2,5 +2,21 @@ namespace DownloadManager.Core;
 
 public class ProgressTracker
 {
+    private int _completed;
+    private int _failed;
+    private readonly int _total;
     
+    public  ProgressTracker(int total)
+    {
+        _total = total;
+    }
+    
+    public void RecordSuccess() => Interlocked.Increment(ref _completed);
+    
+    public void RecordFailed() => Interlocked.Increment(ref _failed);
+
+    public void Print()
+    {
+        Console.WriteLine($"Progress: {_completed}/{_total} done,  {_failed} failed");
+    }
 }
