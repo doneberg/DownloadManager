614c808842d85426245237431832d0251cc37ecb
Don <donde@Dons-MacBook-Air.local>
2026-03-24 11:36:05 -0700

    DownloadResult Model coded

--- Models/DownloadResult.cs
+++ Models/DownloadResult.cs
@@ -2,5 +2,10 @@ namespace DownloadManager.Models;
 
 public class DownloadResult
 {
-    
+    public string Url { get; init; }
+    public string Destination { get; init; }
+    public bool Success { get; init; }
+    public string? ErrorMessage { get; init; }
+    public long BytesDownloaded { get; init; }
+    public TimeSpan Duration { get; init; }
 }
