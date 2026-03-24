50a0586fd90d95939f6f35bebe828ce1dfe48666
Don <donde@Dons-MacBook-Air.local>
2026-03-24 10:45:41 -0700

    DownloadManager coded

--- Core/DownloadManager.cs
+++ Core/DownloadManager.cs
@@ -2,5 +2,31 @@ namespace DownloadManager.Core;
 
 public class DownloadManager
 {
+    private readonly int _maxConcurrent;
     
+    public DownloadManager(int maxConcurrent = 3)
+    {
+        _maxConcurrent = maxConcurrent;
+    }
+
+    public async Task RunAllAsync(List<(string url, string dest)> downloads)
+    {
+        using var semaphore = new SemaphoreSlim(_maxConcurrent);
+
+        var tasks = downloads.Select(async d =>
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                using var job = new DownloadJob(d.url, d.dest);
+                await job.RunAsync();
+                Console.WriteLine($"Done: {d.dest}");
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        });
+        await Task.WhenAll(tasks);
+    }
 }
